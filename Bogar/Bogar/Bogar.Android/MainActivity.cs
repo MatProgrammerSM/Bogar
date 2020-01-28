83686afc9fea11393382362a8d22dc39bcf2425a
figueromefer <ffigueroa@boveda-creativa.com>
2020-01-28 10:46:28 -0600

    splash e icono

--- Bogar/Bogar/Bogar.Android/MainActivity.cs
+++ Bogar/Bogar/Bogar.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Bogar.Droid
 {
-    [Activity(Label = "Bogar", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Bogar",  Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
