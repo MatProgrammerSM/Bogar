3155d389dbac785c2b578acd7b769dac96203b23
figueromefer <ffigueroa@boveda-creativa.com>
2020-01-28 10:27:40 -0600

    Add files via upload

---
+++ Bogar/Bogar/Bogar.Android/SplashActivity.cs
@@ -0,0 +1,15 @@
+﻿
+using Android.App;
+
+namespace Bogar.Droid
+{
+    [Activity(Label = "Bogar", Icon = "@drawable/icon2", Theme = "@style/splashscreen", MainLauncher = true, NoHistory = true)]
+public class SplashActivity : Activity
+{
+    protected override void OnResume()
+    {
+        base.OnResume();
+        StartActivity(typeof(MainActivity));
+    }
+ }
+}
