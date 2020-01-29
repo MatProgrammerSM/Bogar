558cfeeed1969f74ac46baf2a603ef36f81d64a9
figueromefer <ffigueroa@boveda-creativa.com>
2020-01-29 17:19:48 -0600

    * Bogar.Android.csproj: Cambios en Perfil
    * Bogar.csproj:
    * AppDelegate.cs:
    * Perfil.xaml:
    * Bogar.iOS.csproj:
    * Perfil.xaml.cs:
    * MainActivity.cs:
    * imageperfil.png:
    * Resource.designer.cs:
    * imageperfil.png:

--- Bogar/Bogar/Bogar.Android/MainActivity.cs
+++ Bogar/Bogar/Bogar.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using ImageCircle.Forms.Plugin.Droid;
 
 namespace Bogar.Droid
 {
@@ -19,7 +20,7 @@ namespace Bogar.Droid
             Rg.Plugins.Popup.Popup.Init(this, savedInstanceState);
 
             base.OnCreate(savedInstanceState);
-
+            ImageCircleRenderer.Init();
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
--- Bogar/Bogar/Bogar/Paginas/Perfil.xaml.cs
+++ Bogar/Bogar/Bogar/Paginas/Perfil.xaml.cs
@@ -17,6 +17,47 @@ namespace Bogar
             InitializeComponent();
         }
 
+        public static AbsoluteLayout cargar_menu_inferior()
+        {
+            try
+            {
+                AbsoluteLayout absoluteLayout1 = new AbsoluteLayout();
+                AbsoluteLayout.SetLayoutFlags(absoluteLayout1, AbsoluteLayoutFlags.All);
+                AbsoluteLayout.SetLayoutBounds(absoluteLayout1, new Rectangle(0, 1, 1, 0.1));
+                StackLayout menu = new StackLayout() { HorizontalOptions = LayoutOptions.FillAndExpand, VerticalOptions = LayoutOptions.End, Padding = new Thickness(10, 5, 10, 5), BackgroundColor = Color.FromHex("#ffffff"), Orientation = StackOrientation.Horizontal };
+                AbsoluteLayout.SetLayoutFlags(menu, AbsoluteLayoutFlags.All);
+                AbsoluteLayout.SetLayoutBounds(menu, new Rectangle(1, 1, 1, 1));
+                absoluteLayout1.Children.Add(menu);
+
+                var icono_home = new Image() { HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.Center, Source = ImageSource.FromFile("main.png"), HeightRequest = 32 };
+                menu.Children.Add(icono_home);
+                icono_home.GestureRecognizers.Add(new TapGestureRecognizer { Command = new Command(() => { try { Application.Current.MainPage = new NavigationPage(new Perfil()); } catch (Exception ex) { Application.Current.MainPage.DisplayAlert("Ayuda", ex.Message, "OK"); } }), NumberOfTapsRequired = 1 });
+
+                var icono_nuevo = new Image() { HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.Center, Source = ImageSource.FromFile("location.png"), HeightRequest = 30 };
+                menu.Children.Add(icono_nuevo);
+                icono_nuevo.GestureRecognizers.Add(new TapGestureRecognizer { Command = new Command(() => { try { Application.Current.MainPage.Navigation.PushAsync(new Perfil()); } catch (Exception ex) { Application.Current.MainPage.DisplayAlert("Ayuda", ex.Message, "OK"); } }), NumberOfTapsRequired = 1 });
+
+                var icono_estadisticas = new Image() { HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.Center, Source = ImageSource.FromFile("plus.png"), HeightRequest = 30 };
+                menu.Children.Add(icono_estadisticas);
+                icono_estadisticas.GestureRecognizers.Add(new TapGestureRecognizer { Command = new Command(() => { try { Application.Current.MainPage.Navigation.PushAsync(new NuevoDesarrollo()); } catch (Exception ex) { Application.Current.MainPage.DisplayAlert("Ayuda", ex.Message, "OK"); } }), NumberOfTapsRequired = 1 });
+
+                var icono_menu = new Image() { HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.Center, Source = ImageSource.FromFile("menu.png"), HeightRequest = 30 };
+                menu.Children.Add(icono_menu);
+                icono_menu.GestureRecognizers.Add(new TapGestureRecognizer { Command = new Command(() => { try { Application.Current.MainPage.Navigation.PushAsync(new Desarrollos()); } catch (Exception ex) { Application.Current.MainPage.DisplayAlert("Ayuda", ex.Message, "OK"); } }), NumberOfTapsRequired = 1 });
+
+                var icono_user = new Image() { HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.Center, Source = ImageSource.FromFile("user.png"), HeightRequest = 30 };
+                menu.Children.Add(icono_user);
+                icono_user.GestureRecognizers.Add(new TapGestureRecognizer { Command = new Command(() => { try { Application.Current.MainPage.Navigation.PushAsync(new Perfil()); } catch (Exception ex) { Application.Current.MainPage.DisplayAlert("Ayuda", ex.Message, "OK"); } }), NumberOfTapsRequired = 1 });
+
+                return absoluteLayout1;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
+        }
+
         void NuevoDesarrollo(object sender, EventArgs e)
         {
             ((NavigationPage)this.Parent).PushAsync(new NuevoDesarrollo());
@@ -36,5 +77,6 @@ namespace Bogar
         {
             ((NavigationPage)this.Parent).PushAsync(new Configuracion());
         }
+
     }
 }
